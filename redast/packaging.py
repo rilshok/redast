1329a9cdfb1271ad81cb8fd8313e1d786f8f9077
Vladislav A. Proskurov <rilshok@pm.me>
2022-04-09 03:43:01 +0700

    encryption layout

--- redast/packaging.py
+++ redast/packaging.py
@@ -1,9 +1,14 @@
 import base64
+import os
 import pickle
 import zlib
 from abc import ABC, abstractmethod
+from hashlib import sha256
+from typing import Union
 
 import cloudpickle
+from cryptography.hazmat.primitives import padding
+from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
 
 
 class Packaging(ABC):
@@ -36,23 +41,85 @@ class Pickling(Packaging):
 
 
 class Encryption(Packaging):
+    def __init__(
+        self,
+        *,
+        key: Union[str, bytes] = None,
+        password: Union[str, bytes] = None,
+        seed: int = None,
+    ):
+        if key is not None:
+            if isinstance(key, str):
+                key = base64.urlsafe_b64decode(key)
+            if not isinstance(key, bytes):
+                raise ValueError("key must be represented in bytes")
+
+        self._key = key or base64.urlsafe_b64decode(
+            Encryption.generate_key(password=password, seed=seed)
+        )
+
+    @property
+    def key(self) -> str:
+        return str(base64.urlsafe_b64encode(self._key), "utf-8")
+
+    @staticmethod
+    def _hash(data: bytes) -> bytes:
+        hasher = sha256()
+        hasher.update(data)
+        return hasher.digest()
+
+    @staticmethod
+    def _cipher(key: bytes):
+        return Cipher(algorithms.AES(key), modes.ECB())
+
+    @staticmethod
+    def _padding():
+        return padding.PKCS7(algorithms.AES.block_size)
+
+    @staticmethod
+    def generate_key(password: Union[str, bytes] = None, seed: int = None) -> str:
+        if password is None:
+            return str(base64.urlsafe_b64encode(os.urandom(32)), "utf-8")
+        if seed is None:
+            raise ValueError("when using a password, you must specify the salt seed")
+        if isinstance(password, str):
+            password = password.encode("utf-8")
+        salt = Encryption._hash((seed).to_bytes(16, byteorder="big"))
+        password += salt
+        for _ in range(390_000):
+            password = Encryption._hash(password)
+        return str(base64.urlsafe_b64encode(password), "utf-8")
+
     def forward(self, i):
-        return NotImplemented
+        padder = Encryption._padding().padder()
+        encryptor = Encryption._cipher(self._key).encryptor()
+        padded = padder.update(i) + padder.finalize()
+        return encryptor.update(padded) + encryptor.finalize()
 
     def backward(self, o):
-        return NotImplemented
+        unpadder = Encryption._padding().unpadder()
+        decryptor = Encryption._cipher(self._key).decryptor()
+        padded = decryptor.update(o) + decryptor.finalize()
+        return unpadder.update(padded) + unpadder.finalize()
 
 
 class Encoding(Packaging):
-    pass
+    def __init__(self, encoding="utf-8"):
+        self._encoding = encoding
 
-
-class Base64(Encoding):
     def forward(self, i):
-        return base64.b64encode(i)
+        return str(i, self._encoding)
 
     def backward(self, o):
-        return base64.b64decode(o)
+        return o.encode(self._encoding)
+
+
+class Base64(Packaging):
+    def forward(self, i):
+        return base64.urlsafe_b64encode(i)
+
+    def backward(self, o):
+        return base64.urlsafe_b64decode(o)
 
 
 class Conveyor(Packaging):
