b80dbd515f61180240ad3d45bb1e310507daada2
Vladislav A. Proskurov <rilshok@pm.me>
2022-04-15 21:11:29 +0700

    hash algs register

--- redast/core/hash.py
+++ redast/core/hash.py
@@ -14,3 +14,14 @@ def blake2b(data: bytes, block_size=2 ** 20):
         raise ValueError
     iterator = (data[i : i + block_size] for i in range(0, len(data), block_size))
     return hexdigest(iterator, hashlib.blake2b)
+
+
+registry = dict(
+    blake2b=blake2b,
+)
+
+
+def get_hash_fn(name: str):
+    if name not in registry:
+        raise ValueError
+    return registry[name]
