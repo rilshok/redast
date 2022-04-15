f96d2e7e16b67e7e93689ad68dc71fb093c97c8a
Vladislav A. Proskurov <rilshok@pm.me>
2022-04-15 22:05:15 +0700

    add hashlib fns

--- redast/core/hash.py
+++ redast/core/hash.py
@@ -26,7 +26,18 @@ def get_hexdigest(algorithm):
 
 
 registry = dict(
+    md5=hashlib.md5,
+    sha1=hashlib.sha1,
+    sha224=hashlib.sha224,
+    sha256=hashlib.sha256,
+    sha384=hashlib.sha384,
+    sha512=hashlib.sha512,
+    sha3_224=hashlib.sha3_224,
+    sha3_256=hashlib.sha3_256,
+    sha3_384=hashlib.sha3_384,
+    sha3_512=hashlib.sha3_512,
     blake2b=hashlib.blake2b,
+    blake2s=hashlib.blake2s,
     xxh32=xxhash.xxh32,
     xxh64=xxhash.xxh64,
     xxh128=xxhash.xxh128,
