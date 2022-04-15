34782082b106e810645818b43160401f2c298e57
Vladislav A. Proskurov <rilshok@pm.me>
2022-04-15 20:41:27 +0700

    assert megacloud io types

--- redast/remote/megacloud.py
+++ redast/remote/megacloud.py
@@ -16,7 +16,16 @@ class MegaCloud:
     def _path(self, key: str) -> str:
         return self._root_name + "/" + key
 
+    @staticmethod
+    def _assert_type_key(key):
+        assert isinstance(key, str)
+
+    @staticmethod
+    def _assert_type_data(key):
+        assert isinstance(key, bytes)
+
     def exists(self, key: str) -> bool:
+        MegaCloud._assert_type_key(key)
         path = self._path(key)
         find = self._mega.find(path)
         if find is None:
@@ -24,12 +33,15 @@ class MegaCloud:
         return True
 
     def save(self, key: str, data: bytes) -> bool:
+        MegaCloud._assert_type_key(key)
+        MegaCloud._assert_type_data(data)
         with bytes_to_temp_file(data) as path:
             self._mega.upload(path, self._root, key)
         # FIXME: may not be true
         return True
 
     def load(self, key: str) -> bytes:
+        MegaCloud._assert_type_key(key)
         file_info = self._mega.find(self._path(key))
         with tempfile.TemporaryDirectory() as tempdir:
             temppath = self._mega.download(file_info, dest_path=tempdir)
@@ -38,6 +50,7 @@ class MegaCloud:
         return data
 
     def delete(self, key: str) -> bool:
+        MegaCloud._assert_type_key(key)
         file_info = self._mega.find(self._path(key))
         self._mega.delete(file_info[0])
         # FIXME: may not be true
