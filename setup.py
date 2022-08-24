bbf1ea5c1bdebcd54cbbcdf79b6cc54a2c84accc
rilshok <rilshok@pm.me>
2022-08-24 13:51:55 +0400

    readability

--- setup.py
+++ setup.py
@@ -1,15 +1,17 @@
+"""redast installation script"""
+
 import runpy
 from pathlib import Path
 
-from setuptools import find_packages, setup
+from setuptools import find_packages, setup  # type: ignore
 
-name = "redast"
-short_descriprion = "remote data storage"
-author = "Vladislav A. Proskurov"
-author_email = "rilshok@pm.me"
-url = "https://github.com/rilshok/redast"
-license = "MIT"
-classifiers = [
+NAME = "redast"
+DESCRIPTION = "remote data storage"
+LICENSE = "MIT"
+AUTHOR = "Vladislav A. Proskurov"
+AUTHOR_EMAIL = "rilshok@pm.me"
+URL = "https://github.com/rilshok/redast"
+CLASSIFIERS = [
     "Development Status :: 3 - Alpha",
     "Programming Language :: Python :: 3.8",
     "Programming Language :: Python :: 3.9",
@@ -17,28 +19,29 @@ classifiers = [
     "Operating System :: OS Independent",
 ]
 
-version_path = Path(__file__).resolve().parent / name / "__version__.py"
-version = runpy.run_path(version_path)["__version__"]
+VERSION_PATH = str(Path(__file__).resolve().parent / NAME / "__version__.py")
+VERSION = runpy.run_path(VERSION_PATH)["__version__"]
 
-with open("README.md", "r") as file:
-    long_description = file.read()
+with open("README.md", "r", encoding="utf-8") as file:
+    LONG_DESCRIPTION = file.read()
 
-with open("requirements.txt", encoding="utf-8") as file:
-    requirements = file.read().splitlines()
+with open("requirements.txt", "r", encoding="utf-8") as file:
+    REQUIREMENTS = file.read().splitlines()
+
 
 setup(
-    name=name,
-    author=author,
-    author_email=author_email,
-    descriprion=short_descriprion,
-    long_description=long_description,
+    name=NAME,
+    descriprion=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
     long_description_content_type="text/markdown",
-    license=license,
-    url=url,
-    packages=find_packages(include=(name,)),
+    license=LICENSE,
+    author=AUTHOR,
+    author_email=AUTHOR_EMAIL,
+    version=VERSION,
+    url=URL,
+    packages=find_packages(include=(NAME,)),
     include_package_data=True,
-    version=version,
-    install_requires=requirements,
-    classifiers=classifiers,
+    install_requires=REQUIREMENTS,
+    classifiers=CLASSIFIERS,
     python_requires=">=3.8",
 )
