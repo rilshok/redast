3e84f77dc5a5bef269d6aabce0fe7adf617634d7
Vladislav A. Proskurov <rilshok@pm.me>
2023-05-28 00:14:12 +0400

    MEGA is excluded

--- redast/__version__.py
+++ redast/__version__.py
@@ -2,4 +2,4 @@
 # Copyright (c) 2022 Vladislav A. Proskurov
 # see LICENSE for full details
 
-__version__ = "0.1.7"
+__version__ = "0.1.8"
--- redast/remote/__init__.py
+++ redast/remote/__init__.py
@@ -1,5 +1,3 @@
 # The MIT License (MIT)
 # Copyright (c) 2022 Vladislav A. Proskurov
 # see LICENSE for full details
-
-from .megacloud import *
